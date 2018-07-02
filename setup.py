6892a82f09a27ac95b0126bb7f1bb694f77577ef
AssortedFantasy <jehanzeb.mirza@yahoo.com>
2018-07-02 10:45:43 -0400

    Everything was broken because I didn't add comma's!

--- setup.py
+++ setup.py
@@ -2,40 +2,40 @@ from setuptools import setup, find_packages, Extension
 
 # Use DESCRIPTION.md for the PyPi listing.
 with open('DESCRIPTION.md', 'r') as description_file:
-	long_desc = description_file.read()
+    long_desc = description_file.read()
 
 orthtree_extension = Extension("orthtree", sources=["orthtree/orthtree.c"])
 
 setup(
-	# Basic Data
-	name='orthtree'
-	version='0.1.0'
-	
-	# Metadata on PyPi
-	author='AssortedFantasy'
-	author_email='jehanzeb.mirza@yahoo.com'
-	description='fast spacially aware n-dimensional container type'
-	long_description=long_desc
-	license='MIT';
-	keywords='quadtree octtree dimensions fast'
-	url='https://github.com/AssortedFantasy/orthtrees'
-	classifiers=[
-		'Development Status :: 3 - Alpha',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.6',
-		
-		'Operating System :: Microsoft :: Windows',
-	],
-	
-	# Packaging and Dependencies
-	packages=find_packages(exclude=['docs', 'tests*'])
-	py_modules=[]
-	ext_modules=[orthtree_extension]
-	
-	# Write the names of dependencies. Ex: numpy, as strings
-	install_requires=[]
-	python_requires='>=3'
+    # Basic Data
+    name='orthtree',
+    version='0.1.0',
+
+    # Metadata on PyPi
+    author='AssortedFantasy',
+    author_email='jehanzeb.mirza@yahoo.com',
+    description='fast spacially aware n-dimensional container type',
+    long_description=long_desc,
+    license='MIT',
+    keywords='quadtree octtree dimensions fast',
+    url='https://github.com/AssortedFantasy/orthtrees',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+
+        'Operating System :: Microsoft :: Windows',
+    ],
+
+    # Packaging and Dependencies
+    packages=find_packages(exclude=['docs', 'tests*']),
+    py_modules=[],
+    ext_modules=[orthtree_extension],
+
+    # Write the names of dependencies. Ex: numpy, as strings
+    install_requires=[],
+    python_requires='>=3',
 )
